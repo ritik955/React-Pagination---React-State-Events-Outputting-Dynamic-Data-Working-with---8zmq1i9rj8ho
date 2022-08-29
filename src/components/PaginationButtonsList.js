d5f0930f64f68bdc5a1f015b67512a9d33a91f59
Ritik Gupta <61592876+ritik955@users.noreply.github.com>
2022-08-29 16:38:45 +0530

    Add files via upload

---
+++ src/components/PaginationButtonsList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const PaginationButtonsList = () => {
+
+    return (
+        <div className="pagination-buttons-list">
+
+        </div>
+    )
+}
+
+export { PaginationButtonsList }
+
